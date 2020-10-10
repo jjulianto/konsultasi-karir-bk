9f4858ac9688b10fbffd98fb7594b1fa03fa99a7
Julianto08 <jjulianto08@gmail.com>
2020-10-10 22:13:21 +0700

    Added bootstrap folder and setting routing

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,10 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data = [
+			'title' => 'Home'
+		];
+		return view('landing-page/index', $data);
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Views/landing-page/index.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Hello, world!</h1>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
