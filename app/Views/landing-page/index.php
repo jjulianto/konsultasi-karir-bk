c4c43be96b9615ac974bd2c5bc343f62874a4dd5
Julianto08 <jjulianto08@gmail.com>
2020-10-10 22:38:22 +0700

    Add the first page with minor additions

--- app/Views/landing-page/index.php
+++ app/Views/landing-page/index.php
@@ -2,10 +2,33 @@
 
 <?= $this->section('content'); ?>
 <div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>Hello, world!</h1>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                Logo
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mt-2" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto nav nav-pills">
+                    <li class="nav-item align-self-center">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+
+<div class="page page-1">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mb-4">Hello World!</h1>
+            </div>
         </div>
     </div>
 </div>
+
 <?= $this->endSection(); ?>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/bootstrap.min.css">
 
+    <!-- My CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
+
     <title><?= $title; ?></title>
 </head>
 
