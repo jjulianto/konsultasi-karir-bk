638e420acbfa2f037611cc9c51218adf7c6c5e8c
Julianto08 <jjulianto08@gmail.com>
2020-10-27 17:00:10 +0700

    Adding first page on landing page and added footer

--- app/Views/landing-page/index.php
+++ app/Views/landing-page/index.php
@@ -13,8 +13,16 @@
             <div class="collapse navbar-collapse mt-2" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto nav nav-pills">
                     <li class="nav-item align-self-center">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="#">Beranda</a>
                     </li>
+                    <li class="nav-item align-self-center">
+                        <a class="nav-link" href="#">Visi & Misi</a>
+                    </li>
+                    <li class="nav-item align-self-center">
+                        <a class="nav-link" href="#">Guru BK</a>
+                    </li>
+                    <a href="#"><button class="btn btn-register ml-2 mb-1">Sign Up</button></a>
+                    <a href="#"><button class="btn btn-login ml-2">Sign in</button></a>
                 </ul>
             </div>
         </div>
@@ -24,11 +32,26 @@
 <div class="page page-1">
     <div class="container">
         <div class="row">
-            <div class="col">
-                <h1 class="mb-4">Hello World!</h1>
+            <div class="col align-self-center text-one pb-5">
+                <h1>
+                    Konsultasi Karir BK
+                </h1>
+                <h2 class="mb-4">
+                    SMKN 1 Cimahi
+                </h2>
+                <p class="mb-4">
+                    Konsultasi karir BK adalah pemberian bantuan penasehatan tentang karir kepada seorang siswa oleh guru yang memiliki pengetahuan, keterampilan, dan kualifikasi profesional yang memadai. Upaya agar siswa mendapatkan arahan dan bimbingan dalam penyelesaian karir yang diinginkan dan sesuai minat mereka.
+                </p>
+                <a href="#"><button class="btn btn-started">Telusuri</button></a>
+            </div>
+            <div class="col vector-img">
+                <img width="600" src="assets/images/vectors/consulting.jpg" alt="Consulting" />
             </div>
         </div>
     </div>
 </div>
+<footer>
+    <p>Konsultasi Karir BK &copy; 2020, SMKN 1 Cimahi</p>
+</footer>
 
 <?= $this->endSection(); ?>
--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
     <!-- AOS -->
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css">
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- My CSS -->
-    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 
     <title><?= $title; ?></title>
 </head>
@@ -29,7 +29,7 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
         AOS.init();
